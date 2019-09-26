0d1a3cf3415dcd536dbb3681227f645d24efa420
Jason T Alborough <jtalborough@gmail.com>
2019-09-26 07:10:36 -0400

    Port of the LutronQuantum class and bridge from essentials v1.4.24

---
+++ PDT.LutronQuantum.EPI/LutronQuantumBridge.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Crestron.SimplSharp;
+using Crestron.SimplSharpPro.DeviceSupport;
+
+using PepperDash.Core;
+using PepperDash.Essentials.Core;
+using PepperDash.Essentials.Bridges;
+using Newtonsoft.Json;
+
+namespace LutronQuantum
+{
+	public static class LutronQuantumTemplateBridge
+	{
+		public static void LinkToApiExt(this LutronQuantum DspDevice, BasicTriList trilist, uint joinStart, string joinMapKey)
+		{
+
+			LutronQuantumConfigObject joinMap = new LutronQuantumConfigObject();
+
+			var JoinMapSerialized = JoinMapHelper.GetJoinMapForDevice(joinMapKey);
+
+			if (!string.IsNullOrEmpty(JoinMapSerialized))
+				joinMap = JsonConvert.DeserializeObject<LutronQuantumConfigObject>(JoinMapSerialized);
+
+		}
+	}
+	public class EssentialsPluginTemplateBridgeJoinMap : JoinMapBase
+	{
+		public EssentialsPluginTemplateBridgeJoinMap()
+		{
+		}
+
+		public override void OffsetJoinNumbers(uint joinStart)
+		{
+		}
+	}
+}
